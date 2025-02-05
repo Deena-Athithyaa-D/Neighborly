416f495d992d5dfd71cf760c8109095800fdf01a
Balaji-3009 <mbalaji3009@gmail.com>
2025-02-05 11:26:26 +0530

    auth and routes

--- api/models.py
+++ api/models.py
@@ -5,6 +5,14 @@ class User(models.Model):
     
     uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     email = models.EmailField(unique=True)
+    
+    def __str__(self):
+        return self.email
+
+class Profile(models.Model):
+    
+    uuid = models.ForeignKey(User, on_delete=models.CASCADE)
+    email = models.EmailField(unique=True)
     name = models.CharField(max_length=50)
     age = models.IntegerField()
     profession = models.CharField(max_length=50)
@@ -29,6 +37,8 @@ class Posts(models.Model):
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     comm_id = models.ForeignKey(Community, on_delete=models.CASCADE)
     text_content = models.CharField(max_length=5000)
+    date = models.DateField(auto_now_add=True)
+    time = models.TimeField(auto_now_add=True)
 
 class Offers(models.Model):
     
--- api/views.py
+++ api/views.py
@@ -4,16 +4,29 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from .serializer import UserSerializer, CommunitySerializer, JoinSerializer, PostsSerializer, OffersSerializer, RequestsSerializer
+from .models import User,Community,Join,Posts,Offers,Requests
+from functools import wraps
+from rest_framework.response import Response
+from rest_framework import status
+from .auth import verify_firebase_token
+
+def firebase_auth_required(view_func):
+    @wraps(view_func)
+    def _wrapped_view(request, *args, **kwargs):
+        try:
+            decoded_token = verify_firebase_token(request)
+            request.user = decoded_token 
+        except Exception as e:
+            return Response({"detail": str(e)}, status=status.HTTP_401_UNAUTHORIZED)
+        return view_func(request, *args, **kwargs)
+    return _wrapped_view
 
 @api_view(['GET','HEAD'])
 def home(request):
     return HttpResponse("Hello World")
 
-@api_view(['GET','HEAD'])
-def test(request):
-    return HttpResponse("Testing.....")
-
 @api_view(['POST'])
+@firebase_auth_required
 def create_user(request):
     new_user = UserSerializer(data = request.data)
     if new_user.is_valid():
@@ -22,6 +35,7 @@ def create_user(request):
     return Response(new_user.errors, status = status.HTTP_400_BAD_REQUEST)
 
 @api_view(['POST'])
+@firebase_auth_required
 def create_community(request):
     new_community = CommunitySerializer(data = request.data)
     if new_community.is_valid():
@@ -29,7 +43,15 @@ def create_community(request):
         return Response(new_community.data, status = status.HTTP_201_CREATED)
     return Response(new_community.errors, status = status.HTTP_400_BAD_REQUEST)
 
+@api_view(['GET'])
+@firebase_auth_required
+def get_user_communities(request, user_id):
+    communities = Join.objects.filter(user_id = user_id)
+    serializer = JoinSerializer(communities, many = True)
+    return Response(serializer.data)
+
 @api_view(['POST'])
+@firebase_auth_required
 def create_join(request):
     new_join = JoinSerializer(data = request.data)
     if new_join.is_valid():
@@ -38,6 +60,7 @@ def create_join(request):
     return Response(new_join.errors, status = status.HTTP_400_BAD_REQUEST)
 
 @api_view(['POST'])
+@firebase_auth_required
 def create_post(request):
     new_post = PostsSerializer(data = request.data)
     if new_post.is_valid():
@@ -45,7 +68,15 @@ def create_post(request):
         return Response(new_post.data, status = status.HTTP_201_CREATED)
     return Response(new_post.errors, status = status.HTTP_400_BAD_REQUEST)
 
+@api_view(['GET'])
+@firebase_auth_required
+def get_posts(request, comm_id):
+    posts = Posts.objects.filter(comm_id= comm_id)
+    serializer = PostsSerializer(posts, many = True)
+    return Response(serializer.data)
+
 @api_view(['POST'])
+@firebase_auth_required
 def create_offer(request):
     new_offer = OffersSerializer(data = request.data)
     if new_offer.is_valid():
@@ -53,11 +84,40 @@ def create_offer(request):
         return Response(new_offer.data, status = status.HTTP_201_CREATED)
     return Response(new_offer.errors, status = status.HTTP_400_BAD_REQUEST)
 
+@api_view(['GET'])
+@firebase_auth_required
+def get_offers(request, comm_id):
+    offers = Offers.objects.filter(comm_id= comm_id)
+    serializer = OffersSerializer(offers, many = True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+@firebase_auth_required
+def get_offers_for_user(request, user_id):
+    offers = Offers.objects.filter(user_id = user_id)
+    serializer = OffersSerializer(offers, many = True)
+    return Response(serializer.data)
+
 @api_view(['POST'])
+@firebase_auth_required
 def create_request(request):
     create_request = RequestsSerializer(data = request.data)
     if create_request.is_valid():
         create_request.save()
         return Response(create_request.data, status = status.HTTP_201_CREATED)
     return Response(create_request.errors, status = status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET'])
+@firebase_auth_required
+def view_public_requests(request, provider_id):
+    requests = Requests.objects.filter(provider_id=None)
+    serializer = RequestsSerializer(requests, many = True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+@firebase_auth_required
+def view_request_from_neighbours(request, comm_id):
+    requests = Requests.objects.filter(provider_id=provider_id)
+    serializer = RequestsSerializer(requests, many = True)
+    return Response(serializer.data)
 
