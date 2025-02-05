0cdfe687b9ed02d042b103e2eb61df59ad1ee5fd
Balaji-3009 <mbalaji3009@gmail.com>
2025-02-05 16:34:16 +0530

    updated settings.py

--- neighborly/settings.py
+++ neighborly/settings.py
@@ -65,6 +65,12 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+]
+
+CORS_ALLOW_ALL_ORIGINS = True
+
 ROOT_URLCONF = 'neighborly.urls'
 
 TEMPLATES = [
