71eb893de8ce82ed36c877eb2b1c7a7bd8409df7
Deena Athithyaa D <deenaathithyaa.d2022@vitstudent.ac.in>
2025-02-05 11:44:44 +0530

    5 -pages

--- Frontend/neighborly/MyApp/Pages/AvailabilityUpload.jsx
+++ Frontend/neighborly/MyApp/Pages/AvailabilityUpload.jsx
@@ -12,7 +12,7 @@ import {
 } from "react-native";
 import * as ImagePicker from "expo-image-picker"; // For image upload
 import Icon from "react-native-vector-icons/MaterialIcons";
-import Noaimge from "../assets/noImage.png";
+// import Noaimge from "../assets/noImage.png";
 const AvailabilityUpload = () => {
   const [name, setName] = useState("");
   const [description, setDescription] = useState("");
